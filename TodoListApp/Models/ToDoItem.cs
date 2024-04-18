ed49ff09c6005732416072e886262348f7cfe7f2
Ivan Pranjic <ivan@nephos.eu>
2024-04-18 09:36:08 +0200

    first commit

---
+++ TodoListApp/Models/ToDoItem.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TodoListApp.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
